ff8e68e264e8b83bd86f06c35abdf7bf90b71bbf
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-13 23:29:08 +0200

    btns

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const btns = document.querySelectorAll('.question-btn');
 
 btns.forEach(function (btn) {
-  btn.addEventListener('click', function(e) {
+  btn.addEventListener('click', function (e) {
     // console.log(e.currentTarget.parentElement.parentElement);
     const question = e.currentTarget.parentElement.parentElement;
     question.classList.toggle('show-text');
