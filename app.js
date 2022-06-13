660bbd19e52832548db0eaf368a16b7f9ab69838
Emmanuel Ingwe <ingwemmanuel@gmail.com>
2022-06-13 22:40:47 +0200

    +4

--- app.js
+++ app.js
@@ -17,12 +17,12 @@ questions.forEach(function (question) {
 });
 
 // traversing the dom
-// const btns = document.querySelectorAll('.question-btn');
-//
-// btns.forEach(function (btn) {
-//   btn.addEventListener('click', function(e) {
-//     // console.log(e.currentTarget.parentElement.parentElement);
-//     const question = e.currentTarget.parentElement.parentElement;
-//     question.classList.toggle('show-text');
-//   });
-// });
+const btns = document.querySelectorAll('.question-btn');
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function(e) {
+    // console.log(e.currentTarget.parentElement.parentElement);
+    const question = e.currentTarget.parentElement.parentElement;
+    question.classList.toggle('show-text');
+  });
+});
